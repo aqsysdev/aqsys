41faf9d4aee1dc34bfbe8f1de563a15d9e29c1ba
Toshimasa Namekawa <toshimasanamekawa@mac.com>
2018-09-04 02:15:44 +0900

    debugging in registor

--- db/entry.js
+++ db/entry.js
@@ -144,6 +144,7 @@ function encodePrize(prize) {
 
 function decodeRow(row) {
     console.log("decodeRow:"+JSON.stringify(row));
+    console.log("config:"+JSON.stringify(config));
     row.lname  = katakanaToHiragana((row.lname||"").replace(/　/g," ").trim().split(" ")[0]);
     row.myouji = (row.myouji||"").replace(/　/g," ").trim().split(" ")[0];
     row.fname  = katakanaToHiragana((row.fname||"").replace(/　/g," ").trim().split(" ").pop());
