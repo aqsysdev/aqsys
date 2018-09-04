5d863fb919d591dfc3d66124ba53735a2c1906be
Toshimasa Namekawa <toshimasanamekawa@mac.com>
2018-09-05 01:19:05 +0900

    enhance grade list

--- public/js/entry.js
+++ public/js/entry.js
@@ -104,7 +104,7 @@ $(function(){
       //
       ///////////////////////////////////////////////////////////////////
       alert("here");
-      alert($(that).name);
+      alert($(that));
 
       if($(that).parent().prev().hasclass("entry-cate")) {
         var num=$(that).attr('name');
