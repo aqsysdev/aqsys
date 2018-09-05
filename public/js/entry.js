4d905b0ce5a67f29d149f8e18fd863754d622275
Toshimasa Namekawa <toshimasanamekawa@mac.com>
2018-09-05 20:43:10 +0900

    simplify callbacks

--- public/js/entry.js
+++ public/js/entry.js
@@ -84,7 +84,7 @@ $(function(){
 
   $('#btnEntryCateEditable').on('click', function () {
     var isChecked;
-    isChecked=($(this).attr("aria-pressed") === true);
+    isChecked=($(this).attr("aria-pressed") == "true");
     $(".entry-cate").prop("disabled",isChecked);
   });
 
@@ -96,7 +96,7 @@ $(function(){
 
   $('#btnEntryRaceNumEditable').on('click', function () {
     var isChecked;
-    isChecked=($(this).attr("aria-pressed") === true);
+    isChecked=($(this).attr("aria-pressed") == "true");
     $(".entry-race-num").prop("disabled",isChecked);
   });
 
@@ -108,7 +108,7 @@ $(function(){
 
   $('#btnEntryWaveEditable').on('click', function () {
     var isChecked;
-    isChecked=($(this).attr("aria-pressed") === true)
+    isChecked=($(this).attr("aria-pressed") == "true");
     $(".entry-wave").prop("disabled",isChecked);
   });
   ////////////////////////////////////////////////////////////////////
