c903e29e7aa3dbc15b695fdeb9162076c6c3f042
Toshimasa Namekawa <toshimasanamekawa@mac.com>
2018-09-15 23:15:43 +0900

    fix bug

--- db/entry.js
+++ db/entry.js
@@ -315,11 +315,11 @@ function encodeWave(wave) {
 }
 
 function decodeRacenum(racenum) {
-//  return((!racenum || isNaN(racenum) || racenum*1 == 0 )? "" : ('000'+racenum*1).slice(-3));
+  return((!racenum || isNaN(racenum) || racenum*1 == 0 )? "" : ('000'+racenum*1).slice(-3));
 }
 
 function encodeRacenum(racenum) {
-//  return((!racenum || isNaN(racenum) || racenum*1==0 )? 0 : ('000'+racenum*1).slice(-3));
+  return((!racenum || isNaN(racenum) || racenum*1==0 )? 0 : ('000'+racenum*1).slice(-3));
 }
 
 function decodeTtime(DNF,ttime) {
