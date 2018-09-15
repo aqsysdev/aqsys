0991af61c49f899e178e50cb70ef4e2ae5f576c2
Toshimasa Namekawa <toshimasanamekawa@mac.com>
2018-09-15 23:03:28 +0900

    restart

--- db/entry.js
+++ db/entry.js
@@ -37,7 +37,37 @@ module.exports = {
   katakanaToHiragana,
   hiraganaToKatakana,
   calcAge,
-  decodeRow
+  formTime,
+  reformTime,
+  encodeTime,
+  decodePrize, encodePrize,
+  decodeRow,
+  decodeLname,
+  decodeMyouji,
+  decodeFname,
+  decodeNamae,
+  decodeBirthday,
+  decodeGrade,
+  decodeSex,
+  decodeZip1,
+  decodeZip2,
+  decodeAddress1,
+  decodeAddress2,
+  decodeEmail,
+  decodeLname2,
+  decodeMyouji2,
+  decodeFname2,
+  decodeNamae2,
+  decodeBirthday2,
+  decodeSex2,
+  decodeRegist,
+  decodeStart,
+  decodeConfirmation,
+  decodeCate,
+  decodeWave,
+  decodeRacenum,  encodeRacenum,
+  decodeTtime,
+  decodePrize
 };
 
 
@@ -145,45 +175,156 @@ function encodePrize(prize) {
 function decodeRow(row) {
 //    console.log("decodeRow:"+JSON.stringify(row));
 //    console.log("config:"+JSON.stringify(config));
-    row.lname  = katakanaToHiragana((row.lname||"").replace(/　/g," ").trim().split(" ")[0]);
-    row.myouji = (row.myouji||"").replace(/　/g," ").trim().split(" ")[0];
-    row.fname  = katakanaToHiragana((row.fname||"").replace(/　/g," ").trim().split(" ").pop());
-    row.namae  = (row.namae||"").replace(/　/g," ").trim().split(" ").pop();
-    var birthday = row.birthday.toString().split(" ");
-    row.birthday = ('0000'+birthday[3]).slice(-4)+"/"+
-    ('00'+(months.indexOf(birthday[1],0)+1)).slice(-2)+"/"+
-    ('00'+birthday[2]).slice(-2);
-    row.grade = (row.grade && config.grades[row.grade]) ? config.grades[row.grade] : calcAge(row.birthday, config.basedate)+ "才";
-    row.sex = sex[(row.sex||"M")] || sex.M ;
-    row.zip1 = ('000'+(row.zip1)).slice(-3);
-    row.zip2 = ('0000'+(row.zip2)).slice(-4);
-    row.address1 = (row.address1||"").trim();
-    row.address2 = (row.address2||"").trim();
-    row.email = (row.email||"").trim() || "dummy@domain.com";
-    row.lname2  = katakanaToHiragana((row.lname2||"").replace(/　/g," ").trim().split(" ")[0]);
-    row.myouji2 = (row.myouji2||"").replace(/　/g," ").trim().split(" ")[0];
-    row.fname2  = katakanaToHiragana((row.fname2||"").replace(/　/g," ").trim().split(" ").pop());
-    row.namae2  = (row.namae2||"").replace(/　/g," ").trim().split(" ").pop();
-    var birthday2 = (row.birthday2||"").toString().split(" ");
-    row.birthday2 = row.birthday2 &&
-      ('0000'+birthday2[3]).slice(-4)+"/"+
-      ('00'+(months.indexOf(birthday2[1],0)+1)).slice(-2)+"/"+
-      ('00'+birthday2[2]).slice(-2);
-    row.sex2 = row.sex2 && sex[(row.sex2||"M")];
-    row.regist = row.regist == true ? "checked" : "" ;
-    row.start = row.start == true ? "checked" : "" ;
-    row.confirmation = row.confirmation == true ? "checked" : "" ;
-    row.cate = (row.cate&&config.cate[row.cate])?config.cate[row.cate]:"";
-    row.wave = (row.wave || row.wave*1 != 0) ? ('00' + row.wave*1).slice(-2) : "";
-    row.racenum = (row.racenum || row.racenum*1 != 0 )? ('000'+row.racenum*1).slice(-3) : "";
-    if( row.DNF ) {
-      row.ttime= "DNF";
-    }else{
-      row.ttime = reformTime(row.ttime);
-    }
+    row.lname  = decodeLname(row.lname);
+    row.myouji = decodeMyouji(row.myouji);
+    row.fname  = decodeFname(row.fname);
+    row.namae  = decodeNamae(row.namae);
+    row.birthday = decodeBirthday(row.birthday);
+    row.grade = decodeGrade(row.grade,row.birthday);
+    row.sex = decodeSex(row.sex);
+    row.zip1 = decodeZip1(row.zip1);
+    row.zip2 = decodeZip2(row.zip2);
+    row.address1 = decodeAddress1(row.address1);
+    row.address2 = decodeAddress2(row.address2);
+    row.email = decodeEmail(row.email);
+    row.lname2  = decodeLname2(row.lname2);
+    row.myouji2 = decodeMyouji2(row.myouji2);
+    row.fname2  = decodeFname2(row.fname2);
+    row.namae2  = decodeNamae2(row.namae2);
+    row.birthday2 = decodeBirthday2(row.birthday2);
+    row.sex2 = decodeSex2(row.sex2);
+    row.regist = decodeRegist( row.regist );
+    row.start = decodeStart(row.start);
+    row.confirmation = decodeConfirmation(row.confirmation);
+    row.cate = decodeCate(row.cate);
+    row.wave = decodeWave(row.wave);
+    row.racenum = decodeRacenum(row.racenum);
+    row.ttime = decodeTtime(row.DNF,row.ttime);
     row.prize1= decodePrize(row.prize1);
     row.prize2= decodePrize(row.prize2);
     row.prize3= decodePrize(row.prize3);
+}
+
+function decodeLname(lname) {
+  return(katakanaToHiragana((lname||"").replace(/　/g," ").trim().split(" ")[0]));
+}
+
+function decodeMyouji(myouji) {
+  return((myouji||"").replace(/　/g," ").trim().split(" ")[0]);
+}
+
+function decodeFname(fname) {
+  return(katakanaToHiragana((fname||"").replace(/　/g," ").trim().split(" ").pop()));
+}
+
+function decodeNamae(namae) {
+  return((namae||"").replace(/　/g," ").trim().split(" ").pop());
+}
+
+function decodeBirthday(argBirthday) {
+  var birthday = argBirthday.toString().split(" ");
+  return(
+    ('0000'+birthday[3]).slice(-4)+"/"+
+    ('00'+(monthsArray.indexOf(birthday[1],0)+1)).slice(-2)+"/"+
+    ('00'+birthday[2]).slice(-2)
+  );
+}
+
+function decodeGrade(grade,birthday) {
+  return((grade && config.grades[grade-1]) ? config.grades[grade-1] : calcAge(birthday, config.basedate)+ "才");
+}
+
+function decodeSex(sex) {
+  return(sexList[(sex||"M")] || sexList.M );
+}
+
+function decodeZip1(zip1) {
+  return(('000'+(zip1)).slice(-3));
+}
+
+function decodeZip2(zip2) {
+  return(('0000'+(zip2)).slice(-4));
+}
+
+function decodeAddress1(address1) {
+  return((address1||"").trim());
+}
+
+function decodeAddress2(address2) {
+  return((address2||"").trim());
+}
+
+function decodeEmail(email) {
+  return((email||"").trim() || "dummy@domain.com");
+}
+
+function decodeLname2(lname2) {
+  return(katakanaToHiragana((lname2||"").replace(/　/g," ").trim().split(" ")[0]));
+}
+
+function decodeMyouji2(myouji2) {
+  return((myouji2||"").replace(/　/g," ").trim().split(" ")[0]);
+}
+
+function decodeFname2(fname2) {
+  return( katakanaToHiragana((fname2||"").replace(/　/g," ").trim().split(" ").pop()));
+}
+
+function decodeNamae2(namae2) {
+  return((namae2||"").replace(/　/g," ").trim().split(" ").pop());
+}
+
+function decodeBirthday2(argBirthday2) {
+  var birthday2 = (argBirthday2||"").toString().split(" ");
+  return(
+    argBirthday2 && ('0000'+birthday2[3]).slice(-4)+"/"+
+    ('00'+(monthsArray.indexOf(birthday2[1],0)+1)).slice(-2)+"/"+
+    ('00'+birthday2[2]).slice(-2)
+  );
+}
+
+function decodeSex2(sex2) {
+  return(sex2 && sexList[(sex2||"M")]);
+}
+
+function decodeRegist(regist) {
+  return(regist == true ? "checked" : "");
+}
+
+function decodeStart( start ) {
+  return(start == true ? "checked" : "") ;
+}
+
+function decodeConfirmation( confirmation ) {
+  return(confirmation == true ? "checked" : "");
+}
+
+function decodeCate(cate) {
+  return((cate&&config.cate[cate-1])?config.cate[cate-1]:"");
+}
+
+function decodeWave(wave) {
+  return((wave || wave*1 != 0) ? ('00' + wave*1).slice(-2) : "");
+}
+
+function encodeWave(wave) {
+  return((!wave || isNaN(wave) || wave==0 )? "" : ('00'+wave*1).slice(-2));
+}
+
+function decodeRacenum(racenum) {
+//  return((!racenum || isNaN(racenum) || racenum*1 == 0 )? "" : ('000'+racenum*1).slice(-3));
+}
+
+function encodeRacenum(racenum) {
+//  return((!racenum || isNaN(racenum) || racenum*1==0 )? 0 : ('000'+racenum*1).slice(-3));
+}
+
+function decodeTtime(DNF,ttime) {
+  if( DNF ) {
+    return("DNF");
+  }else{
+    return(reformTime(ttime));
+  }
 }
 
 console.log("db/entry.js end");
