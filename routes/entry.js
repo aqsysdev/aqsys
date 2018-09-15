2eea7563e60eae4b10da00e65d8730a64ed083a2
Toshimasa Namekawa <toshimasanamekawa@mac.com>
2018-09-16 03:26:33 +0900

    challenge coder

--- routes/entry.js
+++ routes/entry.js
@@ -49,7 +49,7 @@ router.get('/', user.ensureAuthenticated, function(req, res){
     });
     console.log('entry5');
     for(var row of entrylist){
-      entry.decodeRow(row);
+      aqsysCoder.decodeRow(row);
     }
     console.log('entry6');
 //    console.log(JSON.stringify(entry.getConfig().grades));
