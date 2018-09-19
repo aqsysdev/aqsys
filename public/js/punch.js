f89f4da9639fb56b583b7b64138cb57de77f64c8
Toshimasa Namekawa <toshimasanamekawa@mac.com>
2018-09-19 21:31:01 +0900

    refine punch

--- public/js/punch.js
+++ public/js/punch.js
@@ -87,7 +87,7 @@ function punchTime(tnum) {
   alert(tnum-parseInt(tnum/2,10)*2,10);
   if(tnum-parseInt(tnum/2,10)*2,10){
     for(var elem of $( "#recordlist").find(".racenum")) {
-      alert(elem.html());
+      alert(elem.text());
       if(elem=="") {
         break;
       }else{
@@ -96,7 +96,7 @@ function punchTime(tnum) {
     }
   }else{
     for(var elem of $( "#recordlist").find(".ftime")) {
-      alert(elem.html());
+      alert(elem.text());
       if(elem=="") {
         break;
       }else{
