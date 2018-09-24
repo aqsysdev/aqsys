ea1e44294f68919e3aa95a23cdf8069b694fab37
Toshimasa Namekawa <toshimasanamekawa@mac.com>
2018-09-25 00:26:12 +0900

    debug in prize

--- public/js/qrread.js
+++ public/js/qrread.js
@@ -1,55 +1,75 @@
 //
 //
-// QR code reader
+//
 //
 //
 
 //alert("websocket begin");
 var HOST;
-var ws;
+var ws=false;
 
 $(function() {
-  HOST = location.origin.replace(/^http/, 'ws');
-  //alert(HOST);
-  ws = new WebSocket(HOST);
 
-  //サーバから受け取るイベント
-  ws.onopen = function () {
-    QRreadConnect(tnum);
-  };  // 接続時
-  ws.onclose =  function (client) {};  // 切断時
-  ws.onmessage = function (event) {
-    addQRread(event);
-  };
+  function openWebSocket() {
+    if(ws==false) {
+      var HOST = location.origin.replace(/^http/, 'ws');
+      //alert(HOST);
+      ws = new WebSocket(HOST);
+      //サーバから受け取るイベント
+      ws.onopen = function () {
+        punchConnect(tnum);
+      };  // 接続時
+      ws.onclose =  function (client) {
+      };  // 切断時
+      ws.onmessage = function (event) {
+        addPunch(event);
+      };
+      ws.onerror = function (event) {
+        location.reload();
+      };
 
-  setInterval(() => {
-    QRreadBreath(tnum);
-  }, 10000);
+    }
+  }
+  openWebSocket();
+//  setInterval(() => {
+//    punchBreath(tnum);
+//  }, 30000);
   $("#message").each(function(){
       $(this).bind('keyup', zen2han(this));
   });
+  $("#msg_list").scrollTop($("#msg_list")[0].scrollHeight);
+
+  $(".page-header").addClass("hidden");
+  $("#page-header-"+tnum).removeClass("hidden");
+  setInterval(showCurrentTime,1000);
+  showCurrentTime();
+
 });
 
-var zen2han = function(e) {
-    var v, old = e.value;
-    return function(){
-        if( old != ( v = e.value ) ){
-            old = v;
-            var str = $(this).val();
-            str = str.replace( /[Ａ-Ｚａ-ｚ０-９－！”＃＄％＆’（）＝＜＞，．？＿［］｛｝＠＾～￥]/g, function(s) {
-                return String.fromCharCode(s.charCodeAt(0) - 65248);
-            });
-            $(this).val(str);
-        }
-    };
-};
 
+function zen2han(e) {
+  var v, old = e.value;
+  return function(){
+      if( old != ( v = e.value ) ){
+          old = v;
+          var str = $(this).val();
+          str = str.replace( /[Ａ-Ｚａ-ｚ０-９－！”＃＄％＆’（）＝＜＞，．？＿［］｛｝＠＾～￥]/g, function(s) {
+              return String.fromCharCode(s.charCodeAt(0) - 65248);
+          });
+          $(this).val(str);
+      }
+  };
+}
+
+function initRecordList(event) {
+}
+//alert("websocket end");
 
 
   //クライアントからイベント送信（イベント名は自由に設定できます）
 
-function QRreadConnect(tnum) {
-//  alert("qrreadConnect");
+function punchConnect(tnum) {
+//  alert("punchConnect");
   var msg = $("#message").val(); //取得
   $("#message").val("");
   var data = {
@@ -60,10 +80,10 @@ function QRreadConnect(tnum) {
 //  alert("end");
 }
 
-function QRreadTime(tnum) {
-//  alert("QRreadTime");
+function punchTime(tnum) {
   var racenum = decodeRacenum($("#message").val());
   var ftime = encodeTime(new Date());
+  var seqnum = $("#recordlist > tbody").children().length;
   $.post("/api/record/"+tnum,
   {
     ftime: ftime,
@@ -77,12 +97,13 @@ function QRreadTime(tnum) {
       racenum: racenum,
       ftime: ftime
   })); // サーバへ送信
+
 }
 
-function QRreadBreath(tnum) {
-//  alert("QRreadBreath");
+
+function punchBreath(tnum) {
+//  alert("punchBreath");
   var msg = $("#message").val(); //取得
-  $("#message").val("");
   var data = {
     type: "punchBreath",
     tnum: tnum
@@ -92,31 +113,36 @@ function QRreadBreath(tnum) {
 }
 
 //jqueryでメッセージを追加
-function addQRread(event) {
+function addPunch(event) {
   var data=JSON.parse(event.data);
-  var d=$("#msg_list");
+  var table=$("#recordlist");
+  var tr;
+  var td;
   if(data.type=="punch") {
-    $(d).append("<div class='msg'>" +
-    data.seqnum + "," +
-    decodeRacenum(data.racenum) + "," +
-    data.ftime + "</div>");
-    seqnum = data.seqnum+1;
-    $(d).scrollTop($(d)[0].scrollHeight);
+    if($("#seqnum-"+data.seqnum).length==0) {
+      $(table).append(
+        "<tr id='record'>"+
+          "<td style='width:40px' align='center' class='seqnum' id='seqnum-"+data.seqnum+"'>"+data.seqnum+"</td>"+
+          "<td style='width:80px' align='center' class='recenum' id='racenum-"+data.seqnum+"'>"+data.racenum+"</td>"+
+          "<td style='width:100px' align='center' class='ftime' id='ftime-"+data.seqnum+"'>"+data.ftime+"</td>"+
+        "</tr>");
+      $("#msg_list").scrollTop($("#msg_list")[0].scrollHeight);
+    }
   }else if(data.type=="punchBreath") {
   }
 }
 //alert("websocket end");
 
-var encodeTime = function(time) {
+function encodeTime(time) {
   return(
     ("00" + (time.getHours()||0)).slice(-2)+":"+
     ("00" + (time.getMinutes()||0)).slice(-2)+":"+
     ("00" + (time.getSeconds()||0)).slice(-2)+"."+
     ("00" + (time.getTime()||0)).slice(-2)
   );
-};
+}
 
-var formTime = function(ms) {
+function formTime(ms) {
     var milisec=new Decimal(ms);
     return(
       ("00"+parseInt(milisec.div(60*60*100),0)%24).slice(-2)+":"+
@@ -124,9 +150,9 @@ var formTime = function(ms) {
       ("00"+parseInt(milisec.div(100),0)%60).slice(-2)+"."+
       ("00"+parseInt(milisec%100,0)).slice(-2)
     );
-};
+}
 
-var reformTime = function(ft) {
+function reformTime(ft) {
   if(ft){
     var ftime=ft.split(/[-:]/).reverse();
     var sec=parseInt(ftime[0]||0);
@@ -140,15 +166,12 @@ var reformTime = function(ft) {
   } else {
     return(ft);
   }
-};
+}
 
+function decodeRacenum(racenum) {
+  return((!racenum || isNaN(racenum) || racenum*1 == 0 )? "" : ('000'+racenum*1).slice(-3));
+}
 
-var decodeRacenum = function(racenum) {
- if( racenum == null ) {
-   return(null);
- }else if( racenum!="" && racenum>=0){
-   return(("000"+racenum).slice(-3));
- }else{
-   return("");
- }
-};
+function showCurrentTime() {
+  $("#currentTime").html(encodeTime(new Date()));
+}
--- routes/record/qrread.js
+++ routes/record/qrread.js
@@ -36,16 +36,46 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+var recordlist=[];
+
+
 router.get('/:tnum(\\d+)', user.ensureAuthenticated, function(req, res){
   record.getAll(req.params.tnum).then(recordlist => {
-    recordlist.forEach(function(row){record.decodeRow(row);});
-    console.log(recordlist.length);
+    //console.log(recordlist);
+    recordlist.forEach(
+      function(row){record.decodeRow(row);}
+    );
+    var firstRow=null;
+    //console.log(recordlist.length);
+    for(var i in recordlist) {
+      //console.log(recordlist[i]);
+      if(recordlist[i].rid==1){
+        firstRow = recordlist[i];
+        recordlist[i].disabled=false;
+        recordlist.splice(i,1);
+      }
+    }
+    //console.log(recordlist.length);
     recordlist = recordlist.filter(function(row){
       return(!(row.disabled||(!row.racenum&&!row.ftime)));
     });
+    //console.log(recordlist.length);
+    recordlist=recordlist.sort(function(a,b) {
+      return(record.calcTime(b.ftime, a.ftime));
+    });
+    //console.log(recordlist.length);
+    if(firstRow) {
+      recordlist.unshift(firstRow);
+    }
+    //console.log(recordlist.length);
+    seqnum = 0;
+    recordlist.forEach(function(row) {
+      row.seqnum = seqnum;
+      seqnum=seqnum+1;
+    });
     seqnum = recordlist.length;
     console.log(seqnum);
-    res.render('record/qrread',{
+    res.render('record/punch',{
       tnum: req.params.tnum,
       recordlist: recordlist,
       seqnum: seqnum
