9c279f173d3284e8ddd7e15d7b6b5a5b53cd4917
Toshimasa Namekawa <toshimasanamekawa@mac.com>
2018-09-24 16:43:35 +0900

    debug in prize

--- public/js/entry.js
+++ public/js/entry.js
@@ -163,6 +163,7 @@ $(function(){
       table.column(2).search("").draw();
     }
     $('#entry-cate-search').html(($("#entrylist").DataTable().column(2).search() || '<a><span class="caret"></span></a>').replace(/^cate/,""));
+    location.reload();
   });
   $('#entry-cate-search').html(($("#entrylist").DataTable().column(2).search() || '<a><span class="caret"></span></a>').replace(/^cate/,""));
 
@@ -260,6 +261,7 @@ $(function(){
         table.column(3).search("").draw();
       }
       $('#entry-wave-search').html(($("#entrylist").DataTable().column(3).search() || '<a><span class="caret"></span></a>').replace(/^wave/,""));
+      location.reload();
     });
     $('#entry-wave-search').html(($("#entrylist").DataTable().column(3).search() || '<a><span class="caret"></span></a>').replace(/^wave/,""));
 
