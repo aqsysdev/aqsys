8b899809abdc4d8b92f37e70f52d05c9e6d622c0
Toshimasa Namekawa <toshimasanamekawa@mac.com>
2018-09-26 21:15:10 +0900

    try camera

--- public/js/qrread.js
+++ public/js/qrread.js
@@ -267,8 +267,8 @@ $(function() {
     var medias = {
       audio : false,
       video : {
-        width: "300px",
-        height: "300px",
+  //      width: "300px",
+  //      height: "300px",
         frameRate: { ideal: 5, max: 15 },
         facingMode : {
           exact : "environment" // リアカメラにアクセス
