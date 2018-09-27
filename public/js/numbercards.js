b4046ea9938871b79224a844f8dc4f5a9276412f
Toshimasa Namekawa <toshimasanamekawa@mac.com>
2018-09-28 00:04:36 +0900

    chuning QRcode

--- public/js/numbercards.js
+++ public/js/numbercards.js
@@ -29,7 +29,7 @@ $(function(){
   $.each($(".qrcode"),function(index,value) {
 
     var code=$(value).attr("name");
-    $(value).qrcode({width: 4, height: 4, text: code});
+    $(value).qrcode({width: 8, height: 8, text: code});
   });
 
 });
