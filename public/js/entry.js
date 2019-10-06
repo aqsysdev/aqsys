5f1a68d815db89d98ed923541149495e25cf5e77
Toshimasa Namekawa <toshimasanamekawa@mac.com>
2019-10-06 16:44:10 +0900

    read QR in entryForm

--- public/js/entry.js
+++ public/js/entry.js
@@ -585,7 +585,7 @@ $(function(){
         //  height: "300px",
           frameRate: { ideal: 5, max: 15 },
           facingMode : {
-            exact : "user" // user 洗濯
+            exact : "environment" // user 洗濯
           }
         }
       };
