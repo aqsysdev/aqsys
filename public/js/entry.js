0adf11f379dc05e82f2528e73f1d64c2a2af721c
Toshimasa Namekawa <toshimasanamekawa@mac.com>
2019-10-07 17:33:54 +0900

    adjusted CSV for entrytable

--- public/js/entry.js
+++ public/js/entry.js
@@ -560,7 +560,7 @@ $(function(){
         location.reload();
       } else {
       //  $(this).addClass("active");
-        $("#entrylist_length").find("select").selectedIndex = 1;
+        $("#entrylist_length").find("select").options[0].selected = true;
         activateCamera();
       }
     });
