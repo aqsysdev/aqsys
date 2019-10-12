7b7356224578c5550c0090d50d2444cb963ef052
Toshimasa Namekawa <toshimasanamekawa@mac.com>
2019-10-12 11:59:49 +0900

    debug unselectable dropdown by iOS

--- public/js/entry.js
+++ public/js/entry.js
@@ -129,7 +129,7 @@ $(function(){
   ////////////////////////////////////////////////////////////////////
 
   $('.dropdown-cate li').on('click', function(){
-    alert($(this).html);
+    alert($(this).html());
     if($(this).html()!=$(this).parent().prev().html()){
       $(this).parent().prev().addClass("unconfirmed");
       $(this).parent().prev().html($(this).html());
